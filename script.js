5c65fc48a1b8c95e63b02d2b01d8c8ae0a836d1a
thomaswknight <51743941+thomaswknight@users.noreply.github.com>
2024-11-12 11:14:10 +0000

    initial working grid

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const grid = document.querySelector("#grid");
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     const gridCol = document.createElement("div");
     gridCol.className = "column"
     grid.appendChild(gridCol);
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 16; j++) {
         const gridElement = document.createElement("div");
-        gridElement.className = "grid-element"
-        gridElement.textContent = "HELLO WORLD"
+        gridElement.className = "grid-item"
         gridCol.appendChild(gridElement)
     }
 }
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+gridItems.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      item.style.background = "red";
+    });
+  });
