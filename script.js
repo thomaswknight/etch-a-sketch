fb37358340a1814892f2f981f6a06a5e03299e48
thomaswknight <51743941+thomaswknight@users.noreply.github.com>
2024-11-12 11:54:27 +0000

    added grid size button

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-const grid = document.querySelector("#grid");
+const grid = document.querySelector("#grid")
+const button = document.querySelector("button")
 
-for (let i = 0; i < 16; i++) {
-    const gridCol = document.createElement("div");
-    gridCol.className = "column"
-    grid.appendChild(gridCol);
-    for (let j = 0; j < 16; j++) {
-        const gridElement = document.createElement("div");
-        gridElement.className = "grid-item"
-        gridCol.appendChild(gridElement)
+drawGrid()
+
+function drawGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
+        const gridCol = document.createElement("div");
+        gridCol.className = "column"
+        grid.appendChild(gridCol)
+        for (let j = 0; j < gridSize; j++) {
+            const gridElement = document.createElement("div");
+            gridElement.className = "grid-item"
+            gridCol.appendChild(gridElement)
+        }
+    }
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach((item) => {
+        item.addEventListener("mouseenter", () => {
+            item.style.background = "red"
+        })
+    });
+}
+
+function refreshGrid() {
+    while (grid.firstChild) {
+        grid.firstChild.remove()
     }
 }
 
 const gridItems = document.querySelectorAll(".grid-item");
-
 gridItems.forEach((item) => {
     item.addEventListener("mouseenter", () => {
-      item.style.background = "red";
-    });
-  });
+      item.style.background = "red"
+    })
+});
+
+button.onclick = () => {
+    let gridSize = prompt("grid size?");
+    console.log(!(gridSize > 0 && gridSize <= 100))
+    if (!(gridSize > 0 && gridSize <= 100)) {
+        alert("must be between 0 and 100")
+        gridSize = 16
+    }
+    refreshGrid()
+    drawGrid(gridSize)
+}
