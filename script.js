b001104882c5c1d1790d1579ecf9d2dbeb4773f2
thomaswknight <51743941+thomaswknight@users.noreply.github.com>
2024-11-12 10:27:02 +0000

    display grid using flex

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const grid = document.querySelector("#grid");
 
+for (let i = 0; i < 4; i++) {
+    const gridCol = document.createElement("div");
+    gridCol.className = "column"
+    grid.appendChild(gridCol);
+    for (let j = 0; j < 4; j++) {
+        const gridElement = document.createElement("div");
+        gridElement.className = "grid-element"
+        gridElement.textContent = "HELLO WORLD"
+        gridCol.appendChild(gridElement)
+    }
+}
